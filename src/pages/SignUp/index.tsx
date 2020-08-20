016c04de2d324fdc103b429e2b9e177af4abb76d
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2020-08-20 23:05:19 +0100

    Signing up to application

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -5,6 +5,7 @@ import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import api from '../../services/api';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -45,9 +46,11 @@ const SignUp: React.FC = () => {
             abortEarly: false, // return all errors, not just the first one
           });
 
-          // await api.post('/users', data);
+          await api.post('/users', data);
 
-          // history.push('/');
+          Alert.alert('Cadastro realizado com sucesso! Você já pode fazer login na aplicação.');
+
+          navigation.goBack();
 
         } catch (err) {
           if (err instanceof Yup.ValidationError) {
